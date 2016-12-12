8ab7b92a47b3ebe5aba3e904f0818e2c7a9ba1ee
HienNguyen <nvhien1992@localhost.localdomain>
2016-12-12 16:01:44 +0700

    add model header file

---
+++ model_validation.h
@@ -0,0 +1,6 @@
+#ifndef __MODEL_VALIDATION_H
+#define __MODEL_VALIDATION_H
+
+#include "optimization_model.h"
+
+#endif //__MODEL_VALIDATION_H
---
+++ optimization_model.h
@@ -0,0 +1,6 @@
+#ifndef __OPTIMIZATION_MODEL_H
+#define __OPTIMIZATION_MODEL_H
+
+#include <stdio.h>
+
+#endif //__OPTIMIZATION_MODEL_H
