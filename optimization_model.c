a7ec8dc5cda9ba9f1739dc54d62aa1daf943cfc3
Hien <nvhien1992@gmail.com>
2017-04-19 08:04:20 +0700

    add gradient

--- optimization_model.c
+++ optimization_model.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "optimization_model.h"
 
 #define DEBUG_EN 1
@@ -14,8 +15,8 @@ static void derivative(const start_point_t *sp, const data_samples_t *d_trn, gra
 
 	int i = 0;
 	for (i = 0; i < d_trn->num_samples; i++) {
-		gv->ga += (sp->a*d_trn->x_data[i] + sp->b - d_trn->t_data[i])*d_trn->x_data[i];
-		gv->gb += sp->a*d_trn->x_data[i] + sp->b - d_trn->t_data[i];
+		gv->ga += (sp->a * d_trn->x_data[i] + sp->b - d_trn->t_data[i]) * d_trn->x_data[i];
+		gv->gb += sp->a * d_trn->x_data[i] + sp->b - d_trn->t_data[i];
 	}
 }
 
@@ -26,4 +27,10 @@ errno_t gradient_descent(start_point_t *sp, const int num_iters, const data_samp
 	grad_vector_t gv;
 	derivative(sp, d_trn, &gv);
 	DEBUG("ga: %.3f, gb: %.3f\n", gv.ga, gv.gb);
+	//normalize grad vector
+	float module_gv = sqrt(pow(gv.ga, 2) + pow(gv.gb, 2));
+	DEBUG("%f\n", module_gv);
+	gv.ga = (float)gv.ga/(float)module_gv;
+	gv.gb = (float)gv.gb/(float)module_gv;
+	DEBUG("%f %f\n", gv.ga, gv.gb);
 }
